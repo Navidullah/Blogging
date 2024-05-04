56fb95895b71fa09b9cdeb0ac91936630ec19af3
komal ,k c~x <komaalfarid@gmail.com>
2024-05-04 07:35:25 +0500

    Adding header section and installing flowbite-react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Dashboard from "./pages/Dashboard.jsx";
 import Projects from "./pages/Projects.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
+import Header from "./components/Header.jsx";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
