accbccc974f957f06f51332dea13d05daf3e049d
komal ,k c~x <komaalfarid@gmail.com>
2024-05-04 10:57:56 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 app.listen(3000, () => {
