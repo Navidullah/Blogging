798999c3b7d00ad05051f57142dc94eaa722277d
komal ,k c~x <komaalfarid@gmail.com>
2024-05-04 09:12:21 +0500

    responsive Navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import {
+  Button,
+  Navbar,
+  NavbarCollapse,
+  NavbarToggle,
+  TextInput,
+} from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { IoMdSearch } from "react-icons/io";
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
-  return <div>Header</div>;
+  const path = useLocation().pathname;
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-extrabold dark:text-white"
+      >
+        <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-2 py-1 text-white rounded-md">
+          HEADLINES
+        </span>
+        HUB
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={IoMdSearch}
+          className="hidden lg:inline"
+        />
+        <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+          <IoMdSearch />
+        </Button>
+      </form>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue" pill>
+            Sign in
+          </Button>
+        </Link>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </NavbarCollapse>
+    </Navbar>
+  );
 };
 
 export default Header;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,19 @@ const flowbite = require("flowbite-react/tailwind");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      fontWeight: {
+        hairline: 100,
+        thin: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+    },
   },
   plugins: [flowbite.plugin()],
 };
